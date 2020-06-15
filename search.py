3c3f70b099068c6de4f3cced9af8456002a01289
leejunejae <65431276+leejunejae@users.noreply.github.com>
2020-06-15 15:12:49 +0900

    Update search.py

--- search.py
+++ search.py
@@ -1,3 +1,10 @@
+################################################
+#프로그램명: 검색 기능
+#작성자 : 이준재
+#수정일 : 2020-06-13
+#팀명 : 파이썬클라스 (4조)
+#프로그램 설명: 검색 대상에 해당하는 쿼리셋 구별
+################################################
 from django.shortcuts import render
 from django.db.models import F
 from products.models import Product
